f7e56af8f12d5885b80300b39c688a5a849f1c60
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-11-09 21:40:17 -0600

    Now using the component library vuestic.dev for Vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,16 @@
 <template>
 
 <h1>Deadman Switch</h1>
-    
+
 </template>
 
 
 <script setup>
+
+import { ref } from 'vue';
+
+let inputValue1 = ref('');
+let inputValue2 = ref('');
 
 </script>
 
