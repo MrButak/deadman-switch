d190eabdf64430dac0d8f02eb10221be5f895e46
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-11-09 21:26:30 -0600

    Removed un needed Vue boilerplate. Added SCSS.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
     plugins: [vue()],
     build: {
         outDir: "../public/dist"
-    }
+    },
+    // To use SCSS/SCSS preprosser with a Vite/Vue. This code is along with installing the node package 'npm i scss --save-dev'
+	css: {
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true
+
+                // Reference for global scss variables
+                // additionalData: `@import "@/assets/global.scss";`, // for importing global scss variables
+			},
+		}
+	},
 })
