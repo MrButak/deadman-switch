f29b0ab1f7270eb4531db189efc3f606bc2ce0f2
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-11-10 20:30:42 -0600

    Fixed import, added helpful messages

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,6 +3,7 @@
 <h2 class="va-h3 va-text-center" style="padding: 8px 0">Login</h2>
 
 <LoginForm />
+<!-- After a successful registration, the user is taken to the login page which display these messages -->
 <span v-if="hasRegistered">
     <RegisterSuccessMessages />
 </span>
