04bc1a976ffc01bca4ec7685cf3eba119915e010
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-11-13 21:55:28 -0600

    Config to deploy to production

--- backend/controllers/switchManager.js
+++ backend/controllers/switchManager.js
@@ -1,4 +1,5 @@
-const { getDeadmanSwitches } = require('../javascripts/databaseManager');
+const { getDeadmanSwitches, insertNewDeadmanSwitch } = require('../javascripts/databaseManager');
+const { validateName, validateEmail } = require('../javascripts/validationManager');
 
 // Function will get the users deadman switches from the DB
 exports.getDeadmanSwitchesWithUserId = async(req, res) => {
@@ -27,11 +28,51 @@ exports.getDeadmanSwitchesWithUserId = async(req, res) => {
 };
 
 exports.createNewSwitch = async (req, res) => {
-
-   
-
-
-
+    // recipientFirstName: '',
+    // recipientLastName: '',
+    // recipientEmail: '',
+    // checkInIntervalInDays: 1,
+    // checkInTime: new Date(),
+    // finalMessage: ''
+    let newSwitchData = '';
+    let userId = '';
     
+    try {
+        let bodyData = Object.keys(req.body)
+        newSwitchData = JSON.parse(bodyData.newSwitchData);
+        userId = JSON.parse(bodyData.userId);
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({status: '500', message: 'Unknown error'});
+    };
+    
+    // Falsy data from req.body
+    if(!newSwitchData || !userId) {
+        return res.status(500).json({status: '500', message: 'Unknown error'});
+    };
 
+    // User input validation
+    if(!validateName(newSwitchData.recipientFirstName) ||
+        !validateName(newSwitchData.recipientLastName) ||
+        !validateEmail(newSwitchData.recipientEmail) ||
+        newSwitchData.checkInIntervalInDays < 0 ||
+        newSwitchData.checkInIntervalInDays > 3 ||
+        new Date(newSwitchData.checkInTime).getTime() < 0 ||
+        !newSwitchData.finalMessage)
+            { 
+                return res.status(400).json({status: '400', message: 'Invalid switch settings'}); 
+            };
+
+    // ****** From this point switch is good to put into db *******
+
+    let switchData = await insertNewDeadmanSwitch(userId, newSwitchData);
+    if(!switchData[0]) {
+        return res.status(500).json({status: '500', message: 'An unknown database error occurred'}); 
+    };
+
+    console.log(switchData[1]);
+
+    // console.log(newSwitchData)
+    // console.log(userId)
 };
--- backend/javascripts/databaseManager.js
+++ backend/javascripts/databaseManager.js
@@ -96,3 +96,39 @@ exports.getDeadmanSwitches = async (userId) => {
         return [false];
     };
 };
+
+// ***********************************************************************************
+// Function will insert a new deadman switch into the DB
+// ***********************************************************************************
+exports.insertNewDeadmanSwitch = async (userId, newSwitchData) => {
+    // id SERIAL PRIMARY KEY,
+    // user_id INTEGER REFERENCES app_users(id) NOT NULL,
+    // created_at TIMESTAMP NOT NULL,
+    // check_in_interval_in_hours INTEGER CHECK (check_in_interval_in_hours > 0) NOT NULL,
+    // check_in_by_time TIMESTAMP NOT NULL,
+    // last_checked_in_at TIMESTAMP NOT NULL,
+    // recipient_email TEXT NOT NULL,
+    // recipient_first_name VARCHAR(255) NOT NULL,
+    // recipient_last_name VARCHAR(255) NOT NULL,
+    // final_message TEXT NOT NULL,
+    // triggered BOOLEAN NOT NULL
+    
+
+    // recipientFirstName: '',
+    // recipientLastName: '',
+    // recipientEmail: '',
+    // checkInIntervalInDays: 1,
+    // checkInTime: new Date(),
+    // finalMessage: ''
+    let dbStmt = 'INSERT INTO deadman_switches (user_id, created_at, check_in_by_time, last_checked_in_at, recipient_email, recipient_first_name, recipient_last_name, final_message, triggered) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;';
+    let dbValues = [userId, new Date(Date.now()), newSwitchData.checkInTime, new Date(Date.now()), newSwitchData.recipientEmail,  newSwitchData.firstName,  newSwitchData.lastName,  newSwitchData.finalMessage, false];
+
+    try {
+        let dbQuery = await pool.query(dbStmt, dbValues);
+        return [true, dbQuery.rows[0]];
+    }
+    catch(err) {
+        console.log(err);
+        return [false];
+    };
+};
