5e26ad0703c6d9b6db7c3b47622e4c60c0fa9ed9
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-11-17 17:22:32 -0600

    Debuggin email

--- backend/javascripts/emailManager.js
+++ backend/javascripts/emailManager.js
@@ -118,7 +118,7 @@ exports.sendFinalMessage = (deadmanAccountData, dmSwitch) => {
         templateId: 3,
         params: {
             
-            'recipientName': dmSwitch.recipient_first_name + ' ' + dmSwitch.recipient_last_name,
+            'recipientName': `${dmSwitch.recipient_first_name} ${dmSwitch.recipient_last_name}`,
             'deadmanEmail': deadmanAccountData.email,
             'finalMessage': dmSwitch.final_message,
             'switchCreationDate': dmSwitch.created_at,
@@ -146,62 +146,87 @@ exports.sendFinalMessage = (deadmanAccountData, dmSwitch) => {
 
 exports.sendAlertEmailToDeadman = (deadmanAccountData, dmSwitch) => {
 
-    console.log({deadmanAccountData})
-    console.log({dmSwitch})
-    // Extract just the checkInBytime from the Date Object
-    function extractTimeFromDateObject(dateObj) {
-        
-        let timeString = '';
-        let hoursString = new Date(dateObj).getHours();
-        let minutesString = new Date(dateObj).getMinutes();
-        let secondsString = new Date(dateObj).getSeconds();
-        let millisecondString = new Date(dateObj).getMilliseconds();
-        hoursString = hoursString < 10 ? '0' + hoursString : hoursString
-        minutesString = minutesString < 10 ? '0' + minutesString : minutesString
-        secondsString = secondsString < 10 ? '0' + secondsString : secondsString
-        timeString += hoursString + ':' + minutesString + ':' + secondsString + '.' + millisecondString;
-        return timeString;
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SEND_GRID_API);
+    const msg = {
+    to: dmSwitch.recipient_email,
+    from: 'mspence5555@gmail.com',
+    templateId: 'd-d5a538129abd45789c77ba456183cd90',
+    dynamic_template_data: {
+        'subject': 'Deadman switch tripped.',
+        'name': 'dynamicname'
+        // 'name': `${dmSwitch.recipient_first_name} ${dmSwitch.recipient_last_name}`,
+        // 'recipientName': `${dmSwitch.recipient_first_name} ${dmSwitch.recipient_last_name}`,
+        // 'deadmanEmail': `${deadmanAccountData.email}`,
+        // 'finalMessage': `${dmSwitch.final_message}`,
+        // 'switchCreationDate': `${dmSwitch.created_at}`,
+        // 'checkInIntervalInHours': `${dmSwitch.check_in_interval_in_hours}` 
+    },
     };
-
-
-    let defaultClient = SibApiV3Sdk.ApiClient.instance;
-    let apiKey = defaultClient.authentications['api-key'];
-    apiKey.apiKey = email_sk;
-    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-    let readableCheckInByTime = extractTimeFromDateObject(dmSwitch.check_in_by_time);
-    let firstName = 'Some'
-    let lastName = 'Name'
-
-    sendSmtpEmail = {
-
-        to: [{
-            email: deadmanAccountData.email,
-            name: dmSwitch.recipient_first_name + ' ' + dmSwitch.recipient_last_name
-        }],
-        templateId: 4,
-        // params: {
-            
-        //     'recipientName': dmSwitch.recipient_first_name + ' ' + dmSwitch.recipient_last_name,
-        //     'finalMessage': dmSwitch.final_message,
-        //     'switchName': dmSwitch.switch_name,
-        //     'switchCreationDate': dmSwitch.created_at,
-        //     'checkInIntervalInHours': dmSwitch.check_in_interval_in_hours,
-        //     'checkInByTime': readableCheckInByTime,
-        //     'lastCheckedInAt': dmSwitch.last_checked_in_at
-        // },
-    
-        headers: {
-            'X-Mailin-custom': `api-key: ${process.env.SEND_IN_BLUE_API}|content-type: application/json|accept: application/json`
-        }
-    };
-    
-    apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
-        console.log('email to deadman sent', data)
-        return true;
-    }, 
-    function(error) {
-        console.error(error);
+    sgMail
+    .send(msg)
+    .then(() => { return true })
+    .catch((error) => {
+        console.log(error);
         return false;
     });
+    // console.log({deadmanAccountData})
+    // console.log({dmSwitch})
+    // // Extract just the checkInBytime from the Date Object
+    // function extractTimeFromDateObject(dateObj) {
+        
+    //     let timeString = '';
+    //     let hoursString = new Date(dateObj).getHours();
+    //     let minutesString = new Date(dateObj).getMinutes();
+    //     let secondsString = new Date(dateObj).getSeconds();
+    //     let millisecondString = new Date(dateObj).getMilliseconds();
+    //     hoursString = hoursString < 10 ? '0' + hoursString : hoursString
+    //     minutesString = minutesString < 10 ? '0' + minutesString : minutesString
+    //     secondsString = secondsString < 10 ? '0' + secondsString : secondsString
+    //     timeString += hoursString + ':' + minutesString + ':' + secondsString + '.' + millisecondString;
+    //     return timeString;
+    // };
+
+
+    // let defaultClient = SibApiV3Sdk.ApiClient.instance;
+    // let apiKey = defaultClient.authentications['api-key'];
+    // apiKey.apiKey = email_sk;
+    // let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    // let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    // let readableCheckInByTime = extractTimeFromDateObject(dmSwitch.check_in_by_time);
+    // let firstName = 'Some'
+    // let lastName = 'Name'
+
+    // sendSmtpEmail = {
+
+    //     to: [{
+    //         email: deadmanAccountData.email,
+    //         name: dmSwitch.recipient_first_name + ' ' + dmSwitch.recipient_last_name
+    //     }],
+    //     templateId: 4,
+    //     params: {
+            
+    //         'recipientName': dmSwitch.recipient_first_name + ' ' + dmSwitch.recipient_last_name,
+    //         'finalMessage': dmSwitch.final_message,
+    //         'switchName': dmSwitch.switch_name,
+    //         'switchCreationDate': dmSwitch.created_at,
+    //         'checkInIntervalInHours': dmSwitch.check_in_interval_in_hours,
+    //         'checkInByTime': readableCheckInByTime,
+    //         'lastCheckedInAt': dmSwitch.last_checked_in_at
+    //     },
+    
+    //     headers: {
+    //         'X-Mailin-custom': `api-key: ${process.env.SEND_IN_BLUE_API}|content-type: application/json|accept: application/json`
+    //     }
+    // };
+    
+    // apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+    //     console.log('email to deadman sent', data)
+    //     return true;
+    // }, 
+    // function(error) {
+    //     console.error(error);
+    //     return false;
+    // });
 };
