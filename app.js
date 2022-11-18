cd6ac0b3045e93e3242544114bade19e9fc7a7c5
Matthew Spence <91235721+MrButak@users.noreply.github.com>
2022-11-18 10:11:53 -0600

    Debugging issue where the switch is not being deactivated after it expires

--- app.js
+++ app.js
@@ -34,8 +34,10 @@ const { sendFinalMessage, sendAlertEmailToDeadman } = require('./backend/javascr
 
 async function handleDeadmanSwitchExpired(dmSwitch) {
     // Get the deadman's account information
+    
     let deadmanAccountData = await getUserAccountData(dmSwitch.user_id);
-
+    console.log({deadmanAccountData})
+    console.log('Need this id ^^^^^^')
     // Send an email with their final message to their contact
     let finalMessageSent = await sendFinalMessage(deadmanAccountData, dmSwitch);
     console.log({finalMessageSent})
